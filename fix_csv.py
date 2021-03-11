cd2ada460a5e66986f828cf947fa0282565dc91d
Luis <luis.ribeiro077@gmail.com>
2021-03-11 11:06:49 +0100

    fix for already correct csv

--- fix_csv.py
+++ fix_csv.py
@@ -5,11 +5,17 @@ import re
 
 def fix_csv(name):
     with open(name, 'r') as f:
-        s = f.read().replace(',', '.')
-        s = s.replace(';', ',')
+        s = f.read()
+        if ';' in s:
+            s = s.replace(',', '.')
+            s = s.replace(';', ',')
+        else:
+            print(name, "is already fine")
+            return
         with open(name, 'w') as ff:
             ff.write(s)
             ff.close()
+    print(name, "is now a correct CSV")
 
 def main():
     args = sys.argv[1:]
@@ -21,10 +27,9 @@ def main():
             if bool(re.search(".csv$", file)):
                 try:
                     fix_csv(file)
-                    print("done")
                 except FileNotFoundError:
                     print("Oops!", file, "does not exist")
             else:
-                print(file, "is not a valid csv file")
+                print(file, "is not a valid CSV file")
 
 main()
